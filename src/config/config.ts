5bf4c5a3dc0b0d2595bcea213180a3c60646034e
Haginus <hagiandrei.ah@gmail.com>
2021-04-15 23:06:51 +0300

    added dotenv

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+if(!process.env.PRODUCTION) {
+    dotenv.config();
+}
+
+export const config = {
+    SECRET_KEY: process.env.SECRET_KEY,
+    DATABASE_STRING: `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT}/${process.env.MYSQL_DATABASE}`,
+    MAKE_DELAY: process.env.MAKE_DELAY,
+    PORT: process.env.PORT,
+    WEBSITE_URL: process.env.WEBSITE_URL,
+    mailer: {  
+        host: process.env.MAILER_HOST,
+        port: process.env.MAILER_PORT,
+        auth: !process.env.MAILER_USER ? null : {
+            user: process.env.MAILER_USER,
+            password: process.env.MAILER_PASSWORD
+        }
+    }
+}
+
+console.log(config)
