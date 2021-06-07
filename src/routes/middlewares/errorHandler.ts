b2a7b00ee3374a2fb00d5eae2eb5884c78995770
Haginus <hagiandrei.ah@gmail.com>
2021-06-07 18:55:28 +0300

    fixed a typo

--- src/routes/middlewares/errorHandler.ts
+++ src/routes/middlewares/errorHandler.ts
@@ -8,11 +8,12 @@ export default function () {
         if (err instanceof ValidationError) {
             shownError = new ResponseError(err.errors[0].message);
         } else if (!(err instanceof ResponseError)) {
+            // TODO: Log error
             console.log(err);
             shownError = new ResponseError('A apărut o eroare. Contactați administratorul.', 'INTERNAL_ERROR', 500);
         } else {
             shownError = err;
         }
-        res.status(shownError.httpStatusCode).json(err);
+        res.status(shownError.httpStatusCode).json(shownError);
     }
 }
