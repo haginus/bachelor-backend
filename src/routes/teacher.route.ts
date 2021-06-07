2ec4315b63a358f7ea78a2d81dffd0ca79c78a9e
Haginus <hagiandrei.ah@gmail.com>
2021-06-07 21:07:24 +0300

    refactor routes

--- src/routes/teacher.route.ts
+++ src/routes/teacher.route.ts
@@ -4,166 +4,120 @@ import fileUpload, { UploadedFile } from 'express-fileupload';
 import * as TeacherController from '../controllers/teacher.controller';
 import isLoggedIn from './middlewares/isLoggedIn';
 import isType from './middlewares/isType';
+import isValidated from './middlewares/isValidated';
 
 router.use(isLoggedIn());
 router.use(isType('teacher'));
 
-router.post('/validate', async function (req, res) {
-    try {
-        await TeacherController.validateTeacher(req._user.id);
-        res.json({ success: true });
-    } catch (err) {
-        return res.status(err.status).json(err.code);
-    }
+router.post('/validate', function (req, res, next) {
+    TeacherController.validateTeacher(req._user.id)
+        .then(result => res.json({ success: true }))
+        .catch(err => next(err));
 });
 
-router.get('/offers', async (req, res) => {
-    try {
-        const offers = await TeacherController.getOffers(req._user.id);
-        res.json(offers)
-    } catch(err) {
-        res.status(400).json("BAD_REQUEST");
-    }
+router.use(isValidated());
+
+router.get('/offers', (req, res, next) => {
+    TeacherController.getOffers(req._user.id)
+        .then(offers => res.json(offers))
+        .catch(err => next(err));
 });
 
-router.post('/offers/edit', async (req, res) => {
+router.post('/offers/edit', (req, res, next) => {
     const { id, domainId, topicIds, limit } = req.body;
-    try {
-        const offer = await TeacherController.editOffer(req._user.id, id, domainId, topicIds, limit);
-        res.json(offer)
-    } catch(err) {
-        console.log(err)
-        res.status(400).json(err);
-    }
+    TeacherController.editOffer(req._user.id, id, domainId, topicIds, limit)
+        .then(offer => res.json(offer))
+        .catch(err => next(err));
 });
 
-router.post('/offers/add', async (req, res) => {
+router.post('/offers/add', (req, res, next) => {
     const { domainId, topicIds, limit } = req.body;
-    try {
-        const offer = await TeacherController.addOffer(req._user.id, domainId, topicIds, limit);
-        res.json(offer)
-    } catch(err) {
-        console.log(err)
-        res.status(400).json(err);
-    }
+    TeacherController.addOffer(req._user.id, domainId, topicIds, limit)
+        .then(offer => res.json(offer))
+        .catch(err => next(err));
 });
 
-router.get('/applications', async (req, res) => {
-    let offerId: string | number = <string>req.query.offerId;
-    let state: string = <string>req.query.state;
-    if(offerId) {
-        offerId = parseInt(offerId);
-        if(!isFinite(offerId)) {
-            offerId = null;
-        }
+router.get('/applications', (req, res, next) => {
+    let offerId = parseInt(req.query.offerId as string);
+    let state = req.query.state as string;
+    if(isNaN(offerId)) {
+        offerId = null;
     }
     if(!['accepted', 'declined', 'pending'].includes(state)) {
         state = null;
     }
-    try {
-        const applications = await TeacherController.getApplications(req._user.id, offerId, state);
-        res.json(applications)
-    } catch(err) {
-        console.log(err)
-        res.status(400).json(err);
-    }
+    TeacherController.getApplications(req._user.id, offerId, state)
+        .then(applications => res.json(applications))
+        .catch(err => next(err));
 });
 
-router.post('/applications/decline', async (req, res) => {
+router.post('/applications/decline', (req, res, next) => {
     const { applicationId } = req.body;
-    try {
-        const result = await TeacherController.declineApplication(req._user, applicationId);
-        res.json(result)
-    } catch(err) {
-        console.log(err)
-        res.status(400).json(err);
-    }
+    TeacherController.declineApplication(req._user, applicationId)
+        .then(result => res.json(result))
+        .catch(err => next(err));
 });
 
-router.post('/applications/accept', async (req, res) => {
+router.post('/applications/accept', (req, res, next) => {
     const { applicationId } = req.body;
-    try {
-        const result = await TeacherController.acceptApplication(req._user, applicationId);
-        res.json(result)
-    } catch(err) {
-        console.log(err)
-        res.status(400).json(err);
-    }
+    TeacherController.acceptApplication(req._user, applicationId)
+        .then(result => res.json(result))
+        .catch(err => next(err));
 });
 
-router.get('/domains', async function (req, res) {
-    let domains = await TeacherController.getDomains();
-    res.json(domains);
+router.get('/domains', function (req, res, next) {
+    TeacherController.getDomains()
+        .then(domains => res.json(domains))
+        .catch(err => next(err));
 });
 
-router.get('/papers', async function (req, res) {
-    let papers = await TeacherController.getStudentPapers(req._user);
-    res.json(papers);
+router.get('/papers', function (req, res, next) {
+    TeacherController.getStudentPapers(req._user)
+        .then(papers => res.json(papers))
+        .catch(err => next(err));
 });
 
-router.post('/papers/documents/upload', fileUpload(), async function (req, res) {
+router.post('/papers/documents/upload', fileUpload(), function (req, res, next) {
     const { paperId, perspective, name, type } = req.body;
-    try {
-        let docuemnt = await TeacherController.uploadPaperDocument(req._user, req.files.file as UploadedFile,
-            name, type, perspective, +paperId);
-        res.json(docuemnt);
-    } catch(err) {
-        console.log(err);
-        res.status(400).json(err);
-    }
+    TeacherController.uploadPaperDocument(req._user, req.files.file as UploadedFile,
+        name, type, perspective, +paperId)
+        .then(document => res.json(document))
+        .catch(err => next(err));
 });
 
-router.post('/papers/remove', async function (req, res) {
+router.post('/papers/remove', function (req, res, next) {
     const { paperId } = req.body;
-    try {
-        await TeacherController.removePaper(req._user, +paperId);
-        res.json({ success: true });
-    } catch(err) {
-        console.log(err);
-        res.status(400).json(err);
-    }
+    TeacherController.removePaper(req._user, +paperId)
+        .then(result => res.json(result))
+        .catch(err => next(err));
 });
 
-router.post('/papers/grade', async function (req, res) {
-    try {
-        const { paperId, forPaper, forPresentation } = req.body;
-        await TeacherController.gradePaper(req._user, +paperId, +forPaper, +forPresentation);
-        res.json({ success: true });
-    } catch(err) {
-        console.log(err);
-        res.status(400).json(err);
-    }
+router.post('/papers/grade', function (req, res, next) {
+    const { paperId, forPaper, forPresentation } = req.body;
+    TeacherController.gradePaper(req._user, +paperId, +forPaper, +forPresentation)
+        .then(result => res.json({ success: true }))
+        .catch(err => next(err));
 });
 
-router.get('/committees', async function (req, res) {
-    try {
-        let committees = await TeacherController.getCommittees(req._user);
-        res.json(committees);
-    } catch(err) {
-        console.log(err);
-        res.status(400).json(err);
-    }
+router.get('/committees', function (req, res, next) {
+    TeacherController.getCommittees(req._user)
+        .then(committees => res.json(committees))
+        .catch(err => next(err));
 });
 
-router.get('/committees/:id', async function (req, res) {
-    try {
-        const { id } = req.params;
-        let committee = await TeacherController.getCommitteee(req._user, +id);
-        res.json(committee);
-    } catch(err) {
-        console.log(err);
-        res.status(400).json(err);
-    }
+router.get('/committees/:id', function (req, res, next) {
+    const { id } = req.params;
+    TeacherController.getCommitteee(req._user, +id)
+        .then(committee => res.json(committee))
+        .catch(err => next(err));
 });
 
-router.post('/committees/:id/mark-grades-final', async function (req, res) {
-    try {
-        const { id } = req.params;
-        let result = await TeacherController.markGradesAsFinal(req._user, +id);
-        res.json(result);
-    } catch(err) {
-        res.status(err.httpStatusCode).json(err);
-    }
+router.post('/committees/:id/mark-grades-final', function (req, res, next) {
+    const { id } = req.params;
+    
+    TeacherController.markGradesAsFinal(req._user, +id)
+        .then(result => res.json(result))
+        .catch(err => next(err));
 });
 
 export default router;
