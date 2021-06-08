c6821b62dba71e1c908536f2e937f7eb4610a83c
Haginus <hagiandrei.ah@gmail.com>
2021-06-08 17:22:49 +0300

    update cli

--- cli/add-admin.js
+++ cli/add-admin.js
@@ -1,50 +1,87 @@
 const prompt = require('prompt');
-const { User } = require('../build/models/models');
+const bcrypt = require('bcrypt');
+const util = require('./util');
+
+var lastEnteredPassowrd = '';
 
 const properties = [
     {
         name: 'firstName',
         validator: /^[a-zA-Z\s\-]+$/,
+        required: true,
         warning: 'First name must be only letters, spaces, or dashes',
     },
     {
         name: 'lastName',
         validator: /^[a-zA-Z\s\-]+$/,
+        required: true,
         warning: 'Last name must be only letters, spaces, or dashes',
     },
     {
         name: 'email',
         validator: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+        required: true,
         warning: 'Email format is incorrect.',
     },
     {
         name: 'password',
-        hidden: true
+        hidden: true,
+        required: true,
+        conform: function (value) {
+            lastEnteredPassowrd = value;
+            return value.length >= 8;
+        },
+        warning: 'Password must be at least 8 characters long.',
+        before: function(value) {
+            const salt = bcrypt.genSaltSync();
+            return bcrypt.hashSync(value, salt);
+        }
     },
     {
         name: 'passwordConfirm',
-        hidden: true
+        hidden: true,
+        conform: function (value) {
+            return value === lastEnteredPassowrd;
+        },
+        warning: "Passwords don't match.",
     }
 ];
 
-prompt.start();
+util.connection.on('error', () => {
+    console.log('Unable to connect to database.');
+});    
 
-prompt.get(properties, async function (err, result) {
-    if (err) { return onErr(err); }
-    if(result.password != result.passwordConfirm) {
-        return console.log("Passwords don't match.");
-    }
-    const { firstName, lastName, email, password } = result;
-    let user = { type: 'admin', firstName, lastName, email, password };
-    console.log('Inserting...');
-    await User.create(user);
-    console.log('Done.');
+util.connection.on('connect', () => {
+    prompt.start();
+    prompt.get(properties, async function (err, result) {
+        if (err) { return onErr(err); }
+        const { firstName, lastName, email, password } = result;
+
+        util.connection
+            .execute("INSERT INTO users (firstName, lastName, email, password, validated, type) VALUES (?, ?, ?, ?, 1, 'admin');",
+                [firstName, lastName, email, password])
+                .on('result', () => {
+                    console.log('Admin added succesfully.')
+                    util.connection.end();
+                })
+                .on('error', (err) => {
+                    if(err.code === 'ER_DUP_ENTRY') {
+                        console.log('A user with this email aready exists.');
+                    } else if(err.code === 'ER_NO_SUCH_TABLE') {
+                        console.log('Users table does not exist. Make sure you ran the server for the first time!');
+                    } else {
+                        console.log('An unknown error occured: ', err);
+                    }
+                    util.connection.end();
+                });
+    });
 });
 
 const onErr = (err) => {
-    if(err.message == 'canceled') {
-        console.log('Admin add was canceled.');
+    if (err.message == 'canceled') {
+        console.log('\nAdmin add was canceled.');
     } else {
-        console.log('An error occured.');
+        console.log('\nAn error occured.');
     }
+    util.connection.end();
 }
---
+++ cli/util.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+exports.connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+});
