4484cb1a5149444907cdfa0a0da900f8c0ad79d0
Haginus <hagiandrei.ah@gmail.com>
2021-06-10 01:35:20 +0300

    added constants

---
+++ src/util/constants.ts
@@ -0,0 +1,27 @@
+export const DOMAIN_TYPES = {
+    bachelor: "licență",
+    master: "master"
+}
+
+export const FUNDING_FORMS = {
+    budget: "buget",
+    tax: "taxă"
+}
+
+export const STUDY_FORMS = {
+    if: "Învățământ cu frecvență",
+    ifr: "Învățământ cu frecvență redusă",
+    id: "Învățământ la distanță"
+}
+
+export const USER_TYPES = {
+    student: "student",
+    teacher: "profesor",
+    committee: "comisie",
+    admin: "administrator"
+}
+
+export const PAPER_TYPES = {
+    bachelor: "licență",
+    master: "disertație"
+}
