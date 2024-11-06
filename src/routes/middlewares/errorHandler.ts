48242ae0d27b5ed82ec73153b9444642d836836c
Haginus <hagiandrei.ah@gmail.com>
2024-11-06 18:01:43 +0200

    collect user data when capturing errors

--- src/routes/middlewares/errorHandler.ts
+++ src/routes/middlewares/errorHandler.ts
@@ -3,34 +3,36 @@ import { ValidationError } from "sequelize";
 import { config } from "../../config/config";
 import { ResponseError, ResponseErrorInternal } from "../../util/util";
 
-
-
-
 export default function () {
-    let Sentry: any, Tracing: any;
-    if(config.SENTRY_KEY) {
-        Sentry = require('@sentry/node');
-        Tracing = require('@sentry/tracing');
-        Sentry.init({
-            dsn: config.SENTRY_KEY,
-            tracesSampleRate: 1.0,
-        });
+  let Sentry: any, Tracing: any;
+  if (config.SENTRY_KEY) {
+    Sentry = require('@sentry/node');
+    Tracing = require('@sentry/tracing');
+    Sentry.init({
+      dsn: config.SENTRY_KEY,
+      tracesSampleRate: 1.0,
+    });
+  }
+  return function (err: Error, req: Request, res: Response, next: NextFunction) {
+    function captureException(err: Error) {
+      Sentry?.setUser(req._user ? { id: req._user.id, email: req._user.email } : null);
+      Sentry?.captureException(err);
     }
-    return function (err: Error, req: Request, res: Response, next: NextFunction) {
-        let shownError!: ResponseError;
-        if (err instanceof ValidationError) {
-            shownError = new ResponseError(err.errors[0].message);
-        } else if (!(err instanceof ResponseError)) {
-            Sentry?.captureException(err);
-            console.log(err);
-            shownError = new ResponseError('A apărut o eroare. Contactați administratorul.', 'INTERNAL_ERROR', 500);
-        } else {
-            console.log(err);
-            if(err instanceof ResponseErrorInternal) {
-                Sentry?.captureException(err);
-            }
-            shownError = err;
-        }
-        res.status(shownError.httpStatusCode).json(shownError);
+
+    let shownError!: ResponseError;
+    if (err instanceof ValidationError) {
+      shownError = new ResponseError(err.errors[0].message);
+    } else if (!(err instanceof ResponseError)) {
+      captureException(err);
+      console.log(err);
+      shownError = new ResponseError('A apărut o eroare. Contactați administratorul.', 'INTERNAL_ERROR', 500);
+    } else {
+      console.log(err);
+      if (err instanceof ResponseErrorInternal) {
+        captureException(err);
+      }
+      shownError = err;
     }
+    res.status(shownError.httpStatusCode).json(shownError);
+  }
 }
