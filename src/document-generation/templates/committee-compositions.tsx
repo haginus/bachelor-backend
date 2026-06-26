c82ecc7ba7152d22c29832766414d6db6a826d4b
Haginus <hagiandrei.ah@gmail.com>
2026-06-26 20:05:44 +0300

    fix committee composition group header

--- src/document-generation/templates/committee-compositions.tsx
+++ src/document-generation/templates/committee-compositions.tsx
@@ -48,66 +48,67 @@ const styles = StyleSheet.create({
 export function CommitteeCompositions({ groups, sessionSettings }: CommitteeCompositionsProps) {
   return (
     <Document title="Componență comisii și repartiții pe zile">
-      <Page size="A4" style={globalStyles.page}>
+      <Page size="A4" style={[globalStyles.page, { paddingBottom: '1cm' }]}>
         <Text style={styles.title}>
           Comisiile pentru examenele de finalizare a studiilor{'\n'}Sesiunea {sessionSettings.sessionName}
         </Text>
-        {groups.map((group, index) => {
+        {groups.map((group, groupIndex) => {
           const domains = group[0].domains;
           const domainNames = domains.map((domain) => domain.name).join(', ');
           const domainType = domains[0].type;
           let groupHeader = `${domains.length > 1 ? 'Domeniile' : 'Domeniul'} ${domainNames} - comisii de ${PAPER_TYPES[domainType]}`;
           return (
-            <View key={index} style={styles.group}>
-              <Text style={styles.groupHeader}>
-                {groupHeader}
-              </Text>
-              <View>
-                {group.map((committee, index) => {
-                  const president = committee.members.find(member => member.role == 'president')!;
-                  const secretary = committee.members.find(member => member.role == 'secretary')!;
-                  const members = committee.members.filter(member => member.role == 'member');
-                  const rows: [string, string[]][] = [
-                    ['Președinte', [president.teacher.fullName || '']],
-                    ['Membri', members.map(member => member.teacher.fullName || '')],
-                    ['Secretar', [secretary.teacher.fullName || '']],
-                  ];
-                  return (
-                    <View key={index} style={styles.committee} wrap={false}>
-                      <Text style={styles.committeeTitle}>
-                        {committee.name}
+            <View key={groupIndex} style={styles.group}>
+              {group.map((committee, index) => {
+                const president = committee.members.find(member => member.role == 'president')!;
+                const secretary = committee.members.find(member => member.role == 'secretary')!;
+                const members = committee.members.filter(member => member.role == 'member');
+                const rows: [string, string[]][] = [
+                  ['Președinte', [president.teacher.fullName || '']],
+                  ['Membri', members.map(member => member.teacher.fullName || '')],
+                  ['Secretar', [secretary.teacher.fullName || '']],
+                ];
+                return (
+                  <View key={index} style={styles.committee} wrap={false}>
+                    { /** We render the group header here so that the first committee does not go to the next page without it */}
+                    {index === 0 && (
+                      <Text style={styles.groupHeader}>
+                        {groupHeader}
                       </Text>
-                      {committee.activityDays?.map(day => (
-                        <Text key={day.id} style={{ fontStyle: 'italic' }}>
-                          {[
-                            day.location,
-                            day.startTime?.toLocaleDateString('ro-RO', {
-                              hour: '2-digit',
-                              minute: '2-digit',
-                            })
-                          ].filter(str => !!str).join(' | ')}
+                    )}
+                    <Text style={styles.committeeTitle}>
+                      {committee.name}
+                    </Text>
+                    {committee.activityDays?.map(day => (
+                      <Text key={day.id} style={{ fontStyle: 'italic' }}>
+                        {[
+                          day.location,
+                          day.startTime?.toLocaleDateString('ro-RO', {
+                            hour: '2-digit',
+                            minute: '2-digit',
+                          })
+                        ].filter(str => !!str).join(' | ')}
+                      </Text>
+                    ))}
+                    <View style={{ height: 4 }} />
+                    {rows.map(([position, people], index) => (
+                      <View style={styles.committeeRow} key={index}>
+                        <Text style={styles.committeeRowFirstColumn}>
+                          {position}:
                         </Text>
-                      ))}
-                      <View style={{ height: 4 }} />
-                      {rows.map(([position, people], index) => (
-                        <View style={styles.committeeRow} key={index}>
-                          <Text style={styles.committeeRowFirstColumn}>
-                            {position}:
-                          </Text>
-                          <View style={styles.committeeRowSecondColumn}>
-                            {people.map((name, index) => (
-                              <Text key={index}>
-                                {name}
-                              </Text>
-                            ))}
-                          </View>
+                        <View style={styles.committeeRowSecondColumn}>
+                          {people.map((name, index) => (
+                            <Text key={index}>
+                              {name}
+                            </Text>
+                          ))}
                         </View>
-                      ))}
-                    </View>
-                  );
-                })}
-              </View>
-              {index < groups.length - 1 && <Separator />}
+                      </View>
+                    ))}
+                  </View>
+                );
+              })}
+              {groupIndex < groups.length - 1 && <Separator />}
             </View>
           );
         })}
